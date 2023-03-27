a149f8bc5d47b1beeb8c339be74bac689700ff9e
Bhaskar Rijal <bhaskarrijal13@gmail.com>
2023-03-28 01:22:38 +0545

    update readme

--- README.md
+++ README.md
@@ -1,38 +1,35 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fbhaskarrijal%2Fbhaskarrijal-min&project-name=minimal-portfolio)
+# Bhaskar Rijal's Minimal Portfolio
+
+This is an open source portfolio page for Bhaskar Rijal.
+
+## Features
+
+The portfolio page includes the following features:
+
+-   A responsive layout that looks good on desktop and mobile devices
+-   A list of Bhaskar's projects, with links to their live demos and GitHub repositories
+-   A section highlighting Bhaskar's skills and experience
+
+## Technologies
+
+The portfolio page is built with the following technologies:
+
+-   Next JS
+-   Tailwind CSS
 
 ## Getting Started
 
-First, run the development server:
+To get started with the portfolio page, simply clone the repository and run `npm i && npm run dev` command from the root directory. You can modify the content and styling to suit your needs.
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-```
+## Deployment
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+The portfolio page is deployed on Vercel.
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+## Credits
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.ts`.
+The portfolio page was created by Bhaskar Rijal. Special thanks to the Next JS and Tailwind CSS teams for creating great frameworks for building beautiful websites.
 
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
+## License
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+The portfolio page is licensed under the [MIT license](https://opensource.org/licenses/MIT). Feel free to use and modify the code as you see fit.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,19 @@ import Projects from './components/projects';
 import Inprogress from './components/inprogress';
 import Skills from './components/skills';
 import Social from './components/social';
+import Avatar from '../../assets/sun.jpg';
 
 const Page = () => {
 
 	return (
-		<div className="flex flex-col gap-8 my-16 mx-4 md:mx-0">
+		<div className="flex flex-col gap-8 mx-4 my-16 md:mx-0">
 			<div className="flex flex-col-reverse gap-8 md:flex-row">
 				<div className="flex flex-col gap-4">
-					<h1 className="font-medium text-4xl">
+					<h1 className="text-4xl font-medium">
 						Bhaskar <span className="text-primary">Rijal</span>
 					</h1>
 					<p>
-						Software Engineer | <span className="font-bold">LinkBinary | Open Source</span> <span className="rounded-md text-primary px-2 py-1 text-xs bg-primary/10">
+						Software Engineer | <span className="font-bold">LinkBinary | Open Source</span> <span className="px-2 py-1 text-xs rounded-md text-primary bg-primary/10">
 							<span>NEW</span>
 						</span>
 					</p>
@@ -28,21 +29,21 @@ const Page = () => {
 					</p>
 				</div>
 				<Image
-					src={require('../../assets/sun.jpg')}
+					src={Avatar}
 					alt="Bhaskar Rijal"
-					className="object-cover h-32 w-auto md:w-32 md:h-auto grayscale"
+					className="object-cover w-auto h-32 md:w-32 md:h-auto grayscale"
 					width={300}
 					height={300}
 				/>
 			</div>
 			<div className="flex gap-2">
-				<div className="rounded-md text-primary px-2 text-sm bg-primary/10" aria-label="Blood group: O+VE">
+				<div className="px-2 text-sm rounded-md text-primary bg-primary/10" aria-label="Blood group: O+VE">
 					1999
 				</div>
-				<div className="rounded-md text-primary px-2 text-sm bg-primary/10" aria-label="Blood group: O+VE">
+				<div className="px-2 text-sm rounded-md text-primary bg-primary/10" aria-label="Blood group: O+VE">
 					O+VE
 				</div>
-				<div className="rounded-md text-primary px-2 text-sm bg-primary/10" aria-label="Height: 5 feet 8 and a half inches">
+				<div className="px-2 text-sm rounded-md text-primary bg-primary/10" aria-label="Height: 5 feet 8 and a half inches">
 					<span>5'8½</span>
 				</div>
 			</div>
@@ -58,17 +59,17 @@ const Page = () => {
 			<Social />
 
 			<div className="flex flex-col gap-4">
-				<h2 className="font-medium text-2xl">Contact</h2>
+				<h2 className="text-2xl font-medium">Contact</h2>
 				<ul className="list-inside">
 					<li>
 						<span>{'email: '}</span>
-						<a href="mailto:mail@bhaskarrijal.me" className="text-primary underline italic">
+						<a href="mailto:mail@bhaskarrijal.me" className="italic underline text-primary">
 							mail@bhaskarrijal.me
 						</a>
 					</li>
 					<li>
 						<span>{'full website: '}</span>
-						<a href="https://bhaskarrijal.me" target="_blank" className="text-primary underline italic">
+						<a href="https://bhaskarrijal.me" target="_blank" className="italic underline text-primary">
 							bhaskarrijal.me
 						</a>
 					</li>
@@ -76,7 +77,7 @@ const Page = () => {
 			</div>
 
 			<p className="text-xs">
-				{'Bhaskar Rijal ' + new Date().getFullYear()}
+				{'Bhaskar Rijal ' + new Date().getFullYear()}, <a href='https://github.com/bhaskarrijal/bhaskarrijal-min' target='_blank' className='italic underline text-primary'>edit this site</a>
 			</p>
 		</div>
 	);
